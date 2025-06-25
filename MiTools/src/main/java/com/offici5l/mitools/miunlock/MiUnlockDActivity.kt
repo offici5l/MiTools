cfd932d2ea633dbf388000e81d6b42d81ff3c755
offici5l <127665988+offici5l@users.noreply.github.com>
2025-06-25 21:55:36 +0100

    Update MiUnlockDActivity.kt

--- MiTools/src/main/java/com/offici5l/mitools/miunlock/MiUnlockDActivity.kt
+++ MiTools/src/main/java/com/offici5l/mitools/miunlock/MiUnlockDActivity.kt
@@ -56,7 +56,10 @@ class MiUnlockDActivity : AppCompatActivity() {
                 // deviceToken = MiUnlockFastboot.getDeviceToken(this@MiUnlockDActivity)
 
                 product = "tanzanite"
-                deviceToken = "VQECOwEQ/ oS2UWSpazdbFPRijvObXAMJdGFuemF uaXRIAhyyVTaWKciRI59k2j3BBvsZWaKr Vey+GQ6UBmvL"
+                deviceToken = "VQECOwEQ/
+oS2UWSpazdbFPRijvObXAMJdGFuemF
+uaXRIAhyyVTaWKciRI59k2j3BBvsZWaKr
+Vey+GQ6UBmvL"
 
                 if (product.isNullOrEmpty()) {
                     noticeTextView.text = "Failed to retrieve product."
